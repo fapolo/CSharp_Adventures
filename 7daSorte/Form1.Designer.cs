b92b1d4e9f8e04c6db6b14676444b05cfb33d97a
Fabricio Polo <fabricio.polo@gmail.com>
2019-06-11 10:12:41 -0300

    add game logic

---
+++ 7daSorte/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace _7daSorte
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.Num1 = new System.Windows.Forms.Label();
+            this.Num2 = new System.Windows.Forms.Label();
+            this.Num3 = new System.Windows.Forms.Label();
+            this.ImagemResultado = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagemResultado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Helvetica LT Std", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(260, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Jogo 7 da Sorte";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.Num1);
+            this.panel1.Location = new System.Drawing.Point(211, 83);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(90, 70);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.Num2);
+            this.panel2.Location = new System.Drawing.Point(307, 83);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(90, 70);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.Num3);
+            this.panel3.Location = new System.Drawing.Point(403, 83);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(90, 70);
+            this.panel3.TabIndex = 3;
+            // 
+            // Num1
+            // 
+            this.Num1.AutoSize = true;
+            this.Num1.Font = new System.Drawing.Font("Helvetica LT Std", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num1.Location = new System.Drawing.Point(31, 15);
+            this.Num1.Name = "Num1";
+            this.Num1.Size = new System.Drawing.Size(35, 38);
+            this.Num1.TabIndex = 0;
+            this.Num1.Text = "0";
+            // 
+            // Num2
+            // 
+            this.Num2.AutoSize = true;
+            this.Num2.Font = new System.Drawing.Font("Helvetica LT Std", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num2.Location = new System.Drawing.Point(31, 15);
+            this.Num2.Name = "Num2";
+            this.Num2.Size = new System.Drawing.Size(35, 38);
+            this.Num2.TabIndex = 0;
+            this.Num2.Text = "0";
+            // 
+            // Num3
+            // 
+            this.Num3.AutoSize = true;
+            this.Num3.Font = new System.Drawing.Font("Helvetica LT Std", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num3.Location = new System.Drawing.Point(29, 15);
+            this.Num3.Name = "Num3";
+            this.Num3.Size = new System.Drawing.Size(35, 38);
+            this.Num3.TabIndex = 0;
+            this.Num3.Text = "0";
+            // 
+            // ImagemResultado
+            // 
+            this.ImagemResultado.Location = new System.Drawing.Point(231, 177);
+            this.ImagemResultado.Name = "ImagemResultado";
+            this.ImagemResultado.Size = new System.Drawing.Size(250, 200);
+            this.ImagemResultado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImagemResultado.TabIndex = 4;
+            this.ImagemResultado.TabStop = false;
+            this.ImagemResultado.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Helvetica LT Std", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(102, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 37);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Jogar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Helvetica LT Std", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(676, 376);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(46, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Sair";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 411);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ImagemResultado);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "7 da Sorte";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagemResultado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label Num1;
+        private System.Windows.Forms.Label Num2;
+        private System.Windows.Forms.Label Num3;
+        private System.Windows.Forms.PictureBox ImagemResultado;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 7daSorte/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7daSorte
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            ImagemResultado.Visible = false;
+
+            //Adiciona números aleatórios
+            Random rnd = new Random();
+            Num1.Text = rnd.Next(1, 10).ToString();
+            Num2.Text = rnd.Next(1, 10).ToString();
+            Num3.Text = rnd.Next(1, 10).ToString();
+
+            if (Num1.Text == "7" && Num2.Text == "7" && Num3.Text == "7")
+            {
+                ImagemResultado.Image = Image.FromFile("");
+                ImagemResultado.Visible = true;
+                System.Media.SystemSounds.Beep.Play();
+            } else if (Num1.Text == "7" || Num2.Text == "7" || Num3.Text == "7")
+            {
+                ImagemResultado.Image = Image.FromFile("");
+                ImagemResultado.Visible = true;
+                System.Media.SystemSounds.Beep.Play();
+            } else
+            {
+                ImagemResultado.Image = Image.FromFile("");
+                ImagemResultado.Visible = true;
+            }
+        }
+    }
+}
