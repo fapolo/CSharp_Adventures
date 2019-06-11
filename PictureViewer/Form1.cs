8f1610cd7b0eae12a476b0dffb54f79603c469df
Fabricio Polo <fabricio.polo@gmail.com>
2019-06-11 09:22:31 -0300

    add buttons functions

--- PictureViewer/Form1.cs
+++ PictureViewer/Form1.cs
@@ -16,5 +16,41 @@ namespace PictureViewer
         {
             InitializeComponent();
         }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            // show the Open File dialog. If the user clicks OK, load the chosen picture
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void BackgroundButton_Click(object sender, EventArgs e)
+        {
+            // show color dialog box. If the user clicks OK, change background color
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            //Close the form
+            this.Close();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            // clear picture
+            pictureBox1.Image = null;
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
     }
 }
